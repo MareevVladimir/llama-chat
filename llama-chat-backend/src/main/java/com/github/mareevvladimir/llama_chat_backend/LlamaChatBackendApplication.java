669580d58f9c810a3c69d49ba87ebee5e6eac020
Vladimir <mareev.vladimir95@gmail.com>
2025-11-08 15:17:37 +0100

    Init spring boot backend project

---
+++ llama-chat-backend/src/main/java/com/github/mareevvladimir/llama_chat_backend/LlamaChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.mareevvladimir.llama_chat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamaChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlamaChatBackendApplication.class, args);
+	}
+
+}
---
+++ llama-chat-backend/src/test/java/com/github/mareevvladimir/llama_chat_backend/LlamaChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mareevvladimir.llama_chat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlamaChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
